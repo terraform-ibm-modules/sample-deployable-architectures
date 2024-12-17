3323d1835178e608dbb88f1ae9a734da3bafb11d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-12-17 22:43:09 +0000

    fix(deps): update terraform ibm to latest for deployable architectures (#93)
    * fix(deps): update terraform ibm to latest for deployable architectures
    
    * cda
    
    ---------
    
    Co-authored-by: ocofaigh <ocofaigh@ie.ibm.com>

--- solutions/tf-fullstack-da/version.tf
+++ solutions/tf-fullstack-da/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock deployable architecture into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.71.3"
+      version = "1.72.2"
     }
   }
 }
