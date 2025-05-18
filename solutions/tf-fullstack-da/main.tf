19ef19bd1772a3041f18c4d56c4e025a41d46d8a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-18 11:27:54 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v8.21.25

--- solutions/tf-fullstack-da/main.tf
+++ solutions/tf-fullstack-da/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 # create global COS instance
 module "cos_instance" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.21.24"
+  version           = "8.21.25"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = var.prefix != null ? "${var.prefix}-${var.cos_instance_name}" : var.cos_instance_name
   create_cos_bucket = false
@@ -30,7 +30,7 @@ locals {
 
 module "cos_buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "8.21.24"
+  version = "8.21.25"
   bucket_configs = [
     {
       bucket_name            = local.cos_source_bucket_name
