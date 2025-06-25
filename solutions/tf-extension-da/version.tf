68a150e8d9615ca777f5610d0929af9da479561d
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-06-25 22:27:18 +0100

    Update version.tf

--- solutions/tf-extension-da/version.tf
+++ solutions/tf-extension-da/version.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.9.0"
   required_providers {
     # Lock deployable architecture into an exact provider version - renovate automation will keep it updated
     ibm = {
