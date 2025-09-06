67471d792ebd3c63de73441a1b01e23e2b84f0a2
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-06 11:35:34 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v10.2.14

--- solutions/tf-fullstack-da/main.tf
+++ solutions/tf-fullstack-da/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 # create global COS instance
 module "cos_instance" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.2.13"
+  version           = "10.2.14"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = var.prefix != null ? "${var.prefix}-${var.cos_instance_name}" : var.cos_instance_name
   create_cos_bucket = false
@@ -30,7 +30,7 @@ locals {
 
 module "cos_buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.2.13"
+  version = "10.2.14"
   bucket_configs = [
     {
       bucket_name            = local.cos_source_bucket_name
