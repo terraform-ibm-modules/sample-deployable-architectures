16883d40094639bf7dea1bc2e45ea46ad6818079
Aatreyee-Mukherjee <aatreyee.mukherjee@ibm.com>
2025-12-22 11:44:10 +0530

    added provider visibility

--- solutions/tf-extension-da/variables.tf
+++ solutions/tf-extension-da/variables.tf
@@ -8,6 +8,18 @@ variable "ibmcloud_api_key" {
   sensitive   = true
 }
 
+variable "provider_visibility" {
+  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
+  type        = string
+  # Defaulting this to public to workaround https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5977
+  default = "private"
+
+  validation {
+    condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
+    error_message = "Invalid visibility option. Allowed values are 'public', 'private', or 'public-and-private'."
+  }
+}
+
 variable "cos_instance_guid" {
   type        = string
   description = "The GUID of the Object Storage instance to configure."
