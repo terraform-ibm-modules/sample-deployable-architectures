8ca9888e19b7d0c57de8278fddf2ef19f950d811
Aatreyee-Mukherjee <aatreyee.mukherjee@ibm.com>
2025-12-23 20:08:52 +0530

    fixes

--- solutions/tf-fullstack-da/provider.tf
+++ solutions/tf-fullstack-da/provider.tf
@@ -3,6 +3,7 @@
 ########################################################################################################################
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  visibility       = var.provider_visibility
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  visibility            = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && var.cos_source_bucket_region == "ca-mon") ? "vpe" : null
 }
